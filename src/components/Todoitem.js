b94a9ecd0803f27c80be932a4be73990ce4ac192
Victoria Tranfield <victoria.tranfield@gmail.com>
2020-09-02 15:27:47 +0100

    use of bind keyword

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -3,10 +3,27 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export class Todoitem extends Component {
+    getStyle = () => {
+        return {
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ?
+            'line-through' : 'none'
+        }
+    }
+
+    markComplete(e) {
+        console.log(this.props)
+    }
+
     render() {
         return (
-            <div style = {{ backgroundColor: '#f4f4f4' }}>
-                <p>{ this.props.todo.title }</p>
+            <div style = {this.getStyle()}>
+            <p>
+                <input type="checkbox" onChange={this.markComplete.bind(this)} /> {' '}
+                { this.props.todo.title }
+            </p>
             </div>
         )
     }
